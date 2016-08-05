0acc13d10967cba36cba7f2c304c6e9112b04f2d
heart <xinshuaifeng@126.com>
2016-08-05 16:54:14 +0800

    添加大写文件名

---
+++ app/src/main/java/cn/qatime/player/fragment/FragmentRemedialClassTimeTable2.java
@@ -0,0 +1,21 @@
+package cn.qatime.player.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cn.qatime.player.R;
+
+public class FragmentRemedialClassTimeTable2 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_remedial_class_timetable2, container, false);
+        return view;
+    }
+
+}
