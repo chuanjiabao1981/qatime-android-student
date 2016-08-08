cb544ebbe09d744abf3f93f6272d3701add0a74f
139634zgc <13001253655@163.com>
2016-08-08 21:56:24 +0800

    个人中心

--- app/src/main/java/cn/qatime/player/fragment/Fragment4.java
+++ app/src/main/java/cn/qatime/player/fragment/Fragment4.java
@@ -1,29 +1,20 @@
 package cn.qatime.player.fragment;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-
-import java.util.ArrayList;
-import java.util.List;
-
+import android.widget.TextView;
 import cn.qatime.player.R;
-import cn.qatime.player.activity.ClassTimeTableActivity;
 import cn.qatime.player.activity.PersonalMyOrderActivity;
 import cn.qatime.player.base.BaseFragment;
-import cn.qatime.player.adapter.MyPagerAdapter;
 
 public class Fragment4 extends BaseFragment {
 
 
-    private Button button;
+    private TextView tv;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -33,8 +24,8 @@ public class Fragment4 extends BaseFragment {
     }
 
     private void initview(View view) {
-        button = (Button) view.findViewById(R.id.my_order);
-        button.setOnClickListener(new View.OnClickListener() {
+        tv = (TextView) view.findViewById(R.id.paying);
+        tv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getActivity(), PersonalMyOrderActivity.class);
