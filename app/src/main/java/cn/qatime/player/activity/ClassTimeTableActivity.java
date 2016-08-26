59bbd72efa7e400778c776ee04e6471d14fde6d5
lungtify <15617685965@163.com>
2016-08-26 17:37:59 +0800

    我的课程表日历完成

--- app/src/main/java/cn/qatime/player/activity/ClassTimeTableActivity.java
+++ app/src/main/java/cn/qatime/player/activity/ClassTimeTableActivity.java
@@ -2,26 +2,60 @@ package cn.qatime.player.activity;
 
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.format.DateUtils;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import com.android.volley.VolleyError;
+import com.google.gson.JsonSyntaxException;
 import com.handmark.pulltorefresh.library.PullToRefreshBase;
 import com.handmark.pulltorefresh.library.PullToRefreshListView;
+import com.orhanobut.logger.Logger;
 
 
+import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import cn.qatime.player.R;
+import cn.qatime.player.base.BaseApplication;
+import cn.qatime.player.bean.ClassTimeTableBean;
+import cn.qatime.player.utils.DaYiJsonObjectRequest;
+import cn.qatime.player.utils.UrlUtils;
+import cn.qatime.player.view.MonthDateView;
 import libraryextra.adapter.CommonAdapter;
 import libraryextra.adapter.ViewHolder;
 import cn.qatime.player.base.BaseActivity;
+import libraryextra.bean.RemedialClassBean;
+import libraryextra.utils.DensityUtils;
+import libraryextra.utils.JsonUtils;
+import libraryextra.utils.StringUtils;
+import libraryextra.utils.VolleyErrorListener;
+import libraryextra.utils.VolleyListener;
 
-public class ClassTimeTableActivity extends BaseActivity {
+public class ClassTimeTableActivity extends BaseActivity implements View.OnClickListener {
     private PullToRefreshListView listView;
-    private java.util.List<String> list = new ArrayList<>();
-    private CommonAdapter<String> adapter;
+    private List<ClassTimeTableBean.DataEntity> totalList = new ArrayList<>();
+    private CommonAdapter<ClassTimeTableBean.DataEntity.LessonsEntity> adapter;
+    private List<Integer> alertList = new ArrayList<>();
     private int page = 1;
+    private MonthDateView monthDateView;
+    private SimpleDateFormat parse = new SimpleDateFormat("yyyy-MM-dd");
+
+    private String date = parse.format(new Date());
+    private List<ClassTimeTableBean.DataEntity.LessonsEntity> itemList = new ArrayList<>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,20 +63,68 @@ public class ClassTimeTableActivity extends BaseActivity {
         setContentView(R.layout.activity_class_time_table);
         setTitle(getResources().getString(R.string.all_course));
         initview();
-        list.add("1");
-        list.add("1");
-        list.add("1");
-        list.add("1");
-        list.add("1");
-        list.add("1");
-        list.add("1");
-        list.add("1");
+        initData();
+    }
+
+    /**
+     * 获取联网数据
+     */
+    private void initData() {
+        Map<String, String> map = new HashMap<>();
+        if (!StringUtils.isNullOrBlanK(date)) {
+            map.put("month", date);
+        }
+        DaYiJsonObjectRequest request = new DaYiJsonObjectRequest(UrlUtils.getUrl(UrlUtils.urlPersonalInformation + BaseApplication.getUserId() + "/schedule", map), null,
+                new VolleyListener(ClassTimeTableActivity.this) {
+                    @Override
+                    protected void onSuccess(JSONObject response) {
+                        totalList.clear();
+                        try {
+                            ClassTimeTableBean data = JsonUtils.objectFromJson(response.toString(), ClassTimeTableBean.class);
+                            totalList.addAll(data.getData());
+                            alertList.clear();
+                            for (int i = 0; i < totalList.size(); i++) {
+                                alertList.add(parse.parse("2016-10-10").getDate());
+                                Logger.e(String.valueOf(parse.parse("2016-10-10").getDate()));
+                            }
+                            monthDateView.setDaysHasThingList(alertList);
+                            filterList();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    protected void onError(JSONObject response) {
+                    }
+
+                    @Override
+                    protected void onTokenOut() {
+                        tokenOut();
+                    }
+                }, new VolleyErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                super.onErrorResponse(volleyError);
+            }
+        });
+        addToRequestQueue(request);
+    }
+
+    private void filterList() {
+        itemList.clear();
+        for (int i = 0; i < totalList.size(); i++) {
+            if (date.equals(totalList.get(i).getDate())) {
+                itemList.addAll(totalList.get(i).getLessons());
+                break;
+            }
+        }
+        adapter.notifyDataSetChanged();
     }
 
 
     private void initview() {
         listView = (PullToRefreshListView) findViewById(R.id.list);
-        listView.setMode(PullToRefreshBase.Mode.BOTH);
+        listView.setMode(PullToRefreshBase.Mode.PULL_FROM_START);
         listView.getLoadingLayoutProxy(true, false).setPullLabel(getResources().getString(R.string.pull_to_refresh));
         listView.getLoadingLayoutProxy(false, true).setPullLabel(getResources().getString(R.string.pull_to_load));
         listView.getLoadingLayoutProxy(true, false).setRefreshingLabel(getResources().getString(R.string.refreshing));
@@ -50,26 +132,26 @@ public class ClassTimeTableActivity extends BaseActivity {
         listView.getLoadingLayoutProxy(true, false).setReleaseLabel(getResources().getString(R.string.release_to_refresh));
         listView.getLoadingLayoutProxy(false, true).setReleaseLabel(getResources().getString(R.string.release_to_load));
 
-
-        adapter = new CommonAdapter<String>(this, list, R.layout.item_activity_class_time_table) {
+        adapter = new CommonAdapter<ClassTimeTableBean.DataEntity.LessonsEntity>(this, itemList, R.layout.item_activity_class_time_table) {
             @Override
-            public void convert(ViewHolder helper, String item, int position) {
+            public void convert(ViewHolder helper, ClassTimeTableBean.DataEntity.LessonsEntity item, int position) {
 
             }
         };
         listView.setAdapter(adapter);
 
-        listView.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener2<ListView>() {
+        listView.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener<ListView>() {
             @Override
-            public void onPullDownToRefresh(PullToRefreshBase<ListView> refreshView) {
-                page = 1;
-                listView.onRefreshComplete();
-            }
-
-            @Override
-            public void onPullUpToRefresh(PullToRefreshBase<ListView> refreshView) {
-                page++;
-                listView.onRefreshComplete();
+            public void onRefresh(PullToRefreshBase<ListView> refreshView) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        String label = DateUtils.formatDateTime(ClassTimeTableActivity.this, System.currentTimeMillis(),
+                                DateUtils.FORMAT_SHOW_TIME | DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_ABBREV_ALL);
+                        listView.getLoadingLayoutProxy(true, false).setLastUpdatedLabel(label);
+                        listView.onRefreshComplete();
+                    }
+                }, 300);
             }
         });
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -77,6 +159,44 @@ public class ClassTimeTableActivity extends BaseActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
             }
         });
+
+        ImageView ivLeft = (ImageView) findViewById(R.id.iv_left);
+        ImageView ivRight = (ImageView) findViewById(R.id.iv_right);
+        monthDateView = (MonthDateView) findViewById(R.id.monthDateView);
+        monthDateView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, DensityUtils.dp2px(this, 25) * 7));
+        TextView tvDate = (TextView) findViewById(R.id.date_text);
+        TextView tvToday = (TextView) findViewById(R.id.tv_today);
+        monthDateView.setTextView(tvDate, null);
+        ivLeft.setOnClickListener(this);
+        ivRight.setOnClickListener(this);
+        tvToday.setOnClickListener(this);
+        monthDateView.setDateClick(new MonthDateView.DateClick() {
+            @Override
+            public void onClickOnDate() {
+                date = monthDateView.getmSelYear() + "-" + (monthDateView.getmSelMonth() + 1) + "-" + monthDateView.getmSelDay();
+                filterList();
+            }
+        });
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.iv_left:
+                monthDateView.onLeftClick();
+                date = monthDateView.getmSelYear() + "-" + (monthDateView.getmSelMonth() + 1) + "-" + monthDateView.getmSelDay();
+//                Logger.e(date);
+                initData();
+                break;
+            case R.id.iv_right:
+                monthDateView.onRightClick();
+                Logger.e(String.valueOf(monthDateView.getmSelMonth()));
+                date = monthDateView.getmSelYear() + "-" + (monthDateView.getmSelMonth() + 1) + "-" + monthDateView.getmSelDay();
+                initData();
+                break;
+            case R.id.tv_today:
+                monthDateView.setTodayToView();
+                break;
+        }
+    }
 }
---
+++ app/src/main/java/cn/qatime/player/view/MonthDateView.java
@@ -0,0 +1,312 @@
+package cn.qatime.player.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import cn.qatime.player.utils.DateUtils;
+
+/**
+ * @author luntify
+ * @date 2016/8/26 11:06
+ * @Description
+ */
+public class MonthDateView extends View {
+    private static final int NUM_COLUMNS = 7;
+    private static final int NUM_ROWS = 6;
+    private final ThreadLocal<Paint> mPaint = new ThreadLocal<>();
+    private int mPreColor = Color.parseColor("#cccccc");
+    private int mAfterColor = Color.parseColor("#000000");
+    private int mSelectBGColor = Color.parseColor("#ff9900");
+    private int mCurrentBGColor = Color.parseColor("#cccccc");
+    private int mCurrYear, mCurrMonth, mCurrDay;
+    private int mSelYear, mSelMonth, mSelDay;
+    private int mColumnSize, mRowSize;
+    private DisplayMetrics mDisplayMetrics;
+    private int mDaySize = 18;
+    private TextView tv_date, tv_week;
+    private int weekRow;
+    private int[][] daysString;
+    private int mCircleRadius = 8;
+    private DateClick dateClick;
+    private int mCircleColor = Color.parseColor("#ff0000");
+    private List<Integer> daysHasThingList = new ArrayList<>();
+
+    public MonthDateView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mDisplayMetrics = getResources().getDisplayMetrics();
+        Calendar calendar = Calendar.getInstance();
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);//抗锯齿
+
+        mPaint.set(paint);
+        mCurrYear = calendar.get(Calendar.YEAR);
+        mCurrMonth = calendar.get(Calendar.MONTH);
+        mCurrDay = calendar.get(Calendar.DATE);
+        setSelectYearMonth(mCurrYear, mCurrMonth, mCurrDay);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        initSize();
+        daysString = new int[6][7];
+        mPaint.get().setTextSize(mDaySize * mDisplayMetrics.scaledDensity);
+        String dayString;
+        int mMonthDays = DateUtils.getMonthDays(mSelYear, mSelMonth);
+        int weekNumber = DateUtils.getFirstDayWeek(mSelYear, mSelMonth);
+//        Logger.e("DateView", "DateView:" + mSelMonth+"月1号周" + weekNumber);
+        for (int day = 0; day < mMonthDays; day++) {
+            dayString = (day + 1) + "";
+            int column = (day + weekNumber - 1) % 7;
+            int row = (day + weekNumber - 1) / 7;
+            daysString[row][column] = day + 1;
+            int startX = (int) (mColumnSize * column + (mColumnSize - mPaint.get().measureText(dayString)) / 2);
+            int startY = (int) (mRowSize * row + mRowSize / 2 - (mPaint.get().ascent() + mPaint.get().descent()) / 2);
+            if (dayString.equals(mSelDay + "")) {
+                //绘制背景色矩形
+                int startRecX = mColumnSize * column;
+                int startRecY = mRowSize * row;
+                mPaint.get().setColor(mSelectBGColor);
+                canvas.drawCircle(startRecX + mColumnSize / 2, startRecY + mRowSize / 2, mRowSize / 2, mPaint.get());
+                //记录第几行，即第几周
+                weekRow = row + 1;
+            }
+            //绘制事务圆形标志
+            drawCircle(row, column, day + 1, canvas);
+            if (dayString.equals(mCurrDay + "") && mCurrDay != mSelDay && mCurrMonth == mSelMonth) {
+                //正常月，选中其他日期，则今日为红色
+                int startRecX = mColumnSize * column;
+                int startRecY = mRowSize * row;
+                mPaint.get().setColor(mCurrentBGColor);
+                canvas.drawCircle(startRecX + mColumnSize / 2, startRecY + mRowSize / 2, mRowSize / 2, mPaint.get());
+            }
+            if (mSelYear < mCurrYear || (mSelYear <= mCurrYear && mSelMonth < mCurrMonth) || (mSelYear <= mCurrYear && mSelMonth <= mCurrMonth && Integer.valueOf(dayString) < mCurrDay)) {
+                mPaint.get().setColor(mPreColor);
+            } else {
+                mPaint.get().setColor(mAfterColor);
+            }
+            canvas.drawText(dayString, startX, startY, mPaint.get());
+
+            if (tv_date != null) {
+                tv_date.setText(mSelYear + "年" + (mSelMonth + 1) + "月" + mSelDay + "日");
+            }
+
+            if (tv_week != null) {
+                tv_week.setText("第" + weekRow + "周");
+            }
+        }
+    }
+
+    private void drawCircle(int row, int column, int day, Canvas canvas) {
+        if (daysHasThingList != null && daysHasThingList.size() > 0) {
+            if (!daysHasThingList.contains(day)) return;
+            mPaint.get().setColor(mCircleColor);
+            float circleX = (float) (mColumnSize * column + mColumnSize * 0.8);
+            float circley = (float) (mRowSize * row + mRowSize * 0.2);
+            canvas.drawCircle(circleX, circley, mCircleRadius, mPaint.get());
+        }
+    }
+
+    @Override
+    public boolean performClick() {
+        return super.performClick();
+    }
+
+    private int downX = 0, downY = 0;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int eventCode = event.getAction();
+        switch (eventCode) {
+            case MotionEvent.ACTION_DOWN:
+                downX = (int) event.getX();
+                downY = (int) event.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                break;
+            case MotionEvent.ACTION_UP:
+                int upX = (int) event.getX();
+                int upY = (int) event.getY();
+                if (Math.abs(upX - downX) < 10 && Math.abs(upY - downY) < 10) {//点击事件
+                    performClick();
+                    doClickAction((upX + downX) / 2, (upY + downY) / 2);
+                }
+                break;
+        }
+        return true;
+    }
+
+    /**
+     * 初始化列宽行高
+     */
+    private void initSize() {
+        mColumnSize = getWidth() / NUM_COLUMNS;
+        mRowSize = getHeight() / NUM_ROWS;
+    }
+
+    /**
+     * 设置年月
+     *
+     * @param year
+     * @param month
+     */
+    private void setSelectYearMonth(int year, int month, int day) {
+        mSelYear = year;
+        mSelMonth = month;
+        mSelDay = day;
+    }
+
+    /**
+     * 执行点击事件
+     *
+     * @param x
+     * @param y
+     */
+    private void doClickAction(int x, int y) {
+        int row = y / mRowSize;
+        int column = x / mColumnSize;
+        if (daysString[row][column] != 0) {
+            setSelectYearMonth(mSelYear, mSelMonth, daysString[row][column]);
+            invalidate();
+            //执行activity发送过来的点击处理事件
+            if (dateClick != null) {
+                dateClick.onClickOnDate();
+            }
+        }
+    }
+
+    /**
+     * 左点击，日历向后翻页
+     */
+    public void onLeftClick() {
+        int year = mSelYear;
+        int month = mSelMonth;
+        int day = mSelDay;
+        if (month == 0) {//若果是1月份，则变成12月份
+            year = mSelYear - 1;
+            month = 11;
+        } else if (DateUtils.getMonthDays(year, month) == day) {
+            //如果当前日期为该月最后一点，当向前推的时候，就需要改变选中的日期
+            month = month - 1;
+            day = DateUtils.getMonthDays(year, month);
+        } else {
+            month = month - 1;
+        }
+        setSelectYearMonth(year, month, day);
+        daysHasThingList.clear();
+        invalidate();
+    }
+
+    /**
+     * 右点击，日历向前翻页
+     */
+    public void onRightClick() {
+        int year = mSelYear;
+        int month = mSelMonth;
+        int day = mSelDay;
+        if (month == 11) {//若果是12月份，则变成1月份
+            year = mSelYear + 1;
+            month = 0;
+        } else if (DateUtils.getMonthDays(year, month) == day) {
+            //如果当前日期为该月最后一点，当向前推的时候，就需要改变选中的日期
+            month = month + 1;
+            day = DateUtils.getMonthDays(year, month);
+        } else {
+            month = month + 1;
+        }
+        setSelectYearMonth(year, month, day);
+        daysHasThingList.clear();
+        invalidate();
+    }
+
+    /**
+     * 获取选择的年份
+     *
+     * @return
+     */
+    public int getmSelYear() {
+        return mSelYear;
+    }
+
+    /**
+     * 获取选择的月份
+     *
+     * @return
+     */
+    public int getmSelMonth() {
+        return mSelMonth;
+    }
+
+    /**
+     * 获取选择的日期
+     */
+    public int getmSelDay() {
+        return this.mSelDay;
+    }
+
+
+    /**
+     * 设置显示当前日期的控件
+     *
+     * @param tv_date 显示日期
+     * @param tv_week 显示周
+     */
+    public void setTextView(TextView tv_date, TextView tv_week) {
+        this.tv_date = tv_date;
+        this.tv_week = tv_week;
+        invalidate();
+    }
+
+    /**
+     * 设置事务天数
+     *
+     * @param daysHasThingList
+     */
+    public void setDaysHasThingList(List<Integer> daysHasThingList) {
+        this.daysHasThingList.clear();
+        this.daysHasThingList.addAll(daysHasThingList);
+        invalidate();
+    }
+
+
+    /**
+     * 设置日期的点击回调事件
+     *
+     * @author shiwei.deng
+     */
+    public interface DateClick {
+        public void onClickOnDate();
+    }
+
+    /**
+     * 设置日期点击事件
+     *
+     * @param dateClick
+     */
+    public void setDateClick(DateClick dateClick) {
+        this.dateClick = dateClick;
+    }
+
+    /**
+     * 跳转至今天
+     */
+    public void setTodayToView() {
+        setSelectYearMonth(mCurrYear, mCurrMonth, mCurrDay);
+        invalidate();
+    }
+}
