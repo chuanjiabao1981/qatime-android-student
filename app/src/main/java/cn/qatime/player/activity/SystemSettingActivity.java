b2c6877959850f88f62d2558318a89b86940e2b9
TianhaoranLY <tianhaoran@bjwwtd.com>
2016-08-30 13:30:18 +0800

    QTA-37 下载

--- app/src/main/java/cn/qatime/player/activity/SystemSettingActivity.java
+++ app/src/main/java/cn/qatime/player/activity/SystemSettingActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.orhanobut.logger.Logger;
 
@@ -36,6 +37,7 @@ public class SystemSettingActivity extends BaseActivity implements View.OnClickL
     private Button exit;
     private String totalCacheSize;
     private AlertDialog alertDialog;
+    private String apkUrl;
 
     private void assignViews() {
         learningProcess = (LinearLayout) findViewById(R.id.learning_process);
@@ -110,17 +112,48 @@ public class SystemSettingActivity extends BaseActivity implements View.OnClickL
                 break;
             case R.id.check_update:
                 //TODO 检查版本，进行更新
-                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//                addToRequestQueue(new DaYiJsonObjectRequest(0, "", null, new VolleyListener(this) {
+//                    @Override
+//                    protected void onTokenOut() {
+//
+//                    }
+//
+//                    @Override
+//                    protected void onSuccess(JSONObject response) {
+//
+//                        //TODO 获取更新信信息
+//                        AlertDialog.Builder builder = new AlertDialog.Builder(SystemSettingActivity.this);
+//                        View view = View.inflate(SystemSettingActivity.this, R.layout.dialog_check_update, null);
+//                        Button down = (Button) view.findViewById(R.id.download);
+//                        down.setOnClickListener(SystemSettingActivity.this);
+//                        alertDialog = builder.create();
+//
+//                        alertDialog.show();
+//                        alertDialog.setContentView(view);
+//                        alertDialog.getWindow().setLayout(DensityUtils.dp2px(SystemSettingActivity.this, 300), DensityUtils.dp2px(SystemSettingActivity.this, 500));
+//                    }
+//
+//                    @Override
+//                    protected void onError(JSONObject response) {
+//
+//                    }
+//                }, new Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError volleyError) {
+//
+//                    }
+//                }));
+
                 //TODO 获取更新信信息
-                View view = View.inflate(this, R.layout.dialog_check_update, null);
+                AlertDialog.Builder builder = new AlertDialog.Builder(SystemSettingActivity.this);
+                View view = View.inflate(SystemSettingActivity.this, R.layout.dialog_check_update, null);
                 Button down = (Button) view.findViewById(R.id.download);
-                down.setOnClickListener(this);
+                down.setOnClickListener(SystemSettingActivity.this);
                 alertDialog = builder.create();
 
                 alertDialog.show();
                 alertDialog.setContentView(view);
-                alertDialog.getWindow().setLayout((int) DensityUtils.dp2px(this, 300), (int) DensityUtils.dp2px(this, 500));
-
+                alertDialog.getWindow().setLayout(DensityUtils.dp2px(SystemSettingActivity.this, 300), DensityUtils.dp2px(SystemSettingActivity.this, 500));
                 break;
             case R.id.clean_cache:
                 //TODO 弹出对话框提示
@@ -142,7 +175,47 @@ public class SystemSettingActivity extends BaseActivity implements View.OnClickL
                 startActivity(intent);
                 break;
             case R.id.download:
-                //TODO 更新版
+                //TODO 更新版本
+                Toast.makeText(SystemSettingActivity.this, "开始下载", Toast.LENGTH_SHORT).show();
+                new Thread() {
+                    @Override
+                    public void run() {
+//                FileOutputStream fos = new FileOutputStream(ApkFile);
+//
+//                try {
+//                    URL url = new URL(apkUrl);
+//                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//                    conn.connect();
+//                    apkLength = conn.getContentLength();
+//                    System.out.println();
+//                    InputStream is = conn.getInputStream();
+//                    apkCurrentDownload = 0;
+//                    byte buf[] = new byte[1024];
+//                    int length = -1;
+//                    while ((length = is.read(buf)) != -1) {
+//                        apkCurrentDownload += length;
+//                        progress = (int) (((float) apkCurrentDownload / apkLength) * 100);
+//                        //更新进度
+//                        mHandler.sendEmptyMessage(DOWN_UPDATE);
+//                        fos.write(buf, 0, length);
+//                        if (apkCurrentDownload == apkLength) {
+//                            //下载完成通知安装
+//                            mHandler.sendEmptyMessage(DOWN_OVER);
+//                            break;
+//                        }
+//                        if (interceptFlag) {
+//                            ApkFile.delete();
+//                            break;
+//                        }
+//                    }
+//                    fos.close();
+//                    is.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+                    }
+                }.start();
+
                 alertDialog.dismiss();
                 break;
         }
