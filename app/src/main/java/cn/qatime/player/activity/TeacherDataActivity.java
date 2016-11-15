a2f49793cc1b76b0da651204c085b5060d215829
TianhaoranLY <tianhaoran@bjwwtd.com>
2016-11-15 20:46:49 +0800

    QTA-88 增加相似课程

--- app/src/main/java/cn/qatime/player/activity/TeacherDataActivity.java
+++ app/src/main/java/cn/qatime/player/activity/TeacherDataActivity.java
@@ -17,8 +17,12 @@ import com.orhanobut.logger.Logger;
 
 import org.json.JSONObject;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import cn.qatime.player.R;
 import cn.qatime.player.base.BaseActivity;
@@ -27,6 +31,7 @@ import cn.qatime.player.utils.DaYiJsonObjectRequest;
 import cn.qatime.player.utils.UrlUtils;
 import libraryextra.adapter.CommonAdapter;
 import libraryextra.adapter.ViewHolder;
+import libraryextra.bean.RemedialClassBean;
 import libraryextra.utils.JsonUtils;
 import libraryextra.utils.VolleyErrorListener;
 import libraryextra.utils.VolleyListener;
@@ -44,7 +49,8 @@ public class TeacherDataActivity extends BaseActivity {
     private TextView name;
     private TextView describe;
     private GridViewForScrollView grid;
-    private List<TeacherDataBean.DataBean.Course> list = new ArrayList<>();
+    private List<TeacherDataBean.DataBean.Course> list1 = new ArrayList<>();
+    private List<RemedialClassBean.Data> list2 = new ArrayList<>();
     private TextView teachAge;
     private TextView school;
     private TextView category;
@@ -53,11 +59,14 @@ public class TeacherDataActivity extends BaseActivity {
     private TextView city;
     private TextView town;
     private int page = 0;
-    private CommonAdapter<TeacherDataBean.DataBean.Course> adapter;
+    private CommonAdapter<TeacherDataBean.DataBean.Course> adapter1;
+    private GridViewForScrollView gridSame;
+    private CommonAdapter<RemedialClassBean.Data> adapter2;
+    private PullToRefreshScrollView scroll;
 
     private void assignViews() {
-        PullToRefreshScrollView scroll = (PullToRefreshScrollView) findViewById(R.id.scroll);
-        scroll.setMode(PullToRefreshBase.Mode.DISABLED);
+        scroll = (PullToRefreshScrollView) findViewById(R.id.scroll);
+        scroll.setMode(PullToRefreshBase.Mode.PULL_FROM_END);
         banner = (ImageView) findViewById(R.id.banner);
         headSculpture = (ImageView) findViewById(R.id.head_sculpture);
         sex = (TextView) findViewById(R.id.sex);
@@ -71,10 +80,16 @@ public class TeacherDataActivity extends BaseActivity {
         town = (TextView) findViewById(R.id.town);
         describe = (TextView) findViewById(R.id.describe);
         grid = (GridViewForScrollView) findViewById(R.id.grid);
-        scroll.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener<ScrollView>() {
+        gridSame = (GridViewForScrollView) findViewById(R.id.grid_same);
+        scroll.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener2<ScrollView>() {
             @Override
-            public void onRefresh(PullToRefreshBase<ScrollView> refreshView) {
+            public void onPullDownToRefresh(PullToRefreshBase<ScrollView> refreshView) {
 
+            }
+
+            @Override
+            public void onPullUpToRefresh(PullToRefreshBase<ScrollView> refreshView) {
+                initDataSame(2);
             }
         });
     }
@@ -85,7 +100,12 @@ public class TeacherDataActivity extends BaseActivity {
         setContentView(R.layout.activity_teacher_data);
         assignViews();
         initData(1);
-        adapter = new CommonAdapter<TeacherDataBean.DataBean.Course>(this, list, R.layout.item_teacher_data) {
+        initDataSame(1);
+        initGrid();
+    }
+
+    private void initGrid() {
+        adapter1 = new CommonAdapter<TeacherDataBean.DataBean.Course>(this, list1, R.layout.item_teacher_data) {
 
             @Override
             public void convert(ViewHolder helper, TeacherDataBean.DataBean.Course item, int position) {
@@ -103,11 +123,93 @@ public class TeacherDataActivity extends BaseActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(TeacherDataActivity.this, RemedialClassDetailActivity.class);
-                intent.putExtra("id", list.get(position).getId());
+                intent.putExtra("id", list1.get(position).getId());
                 startActivity(intent);
             }
         });
-        grid.setAdapter(adapter);
+        grid.setAdapter(adapter1);
+
+        adapter2 = new CommonAdapter<RemedialClassBean.Data>(this, list2, R.layout.item_teacher_data) {
+
+            @Override
+            public void convert(ViewHolder helper,RemedialClassBean.Data item, int position) {
+                if (item == null) {
+                    Logger.e("item數據空");
+                    return;
+                }
+                Glide.with(TeacherDataActivity.this).load(item.getPublicize()).placeholder(R.mipmap.photo).centerCrop().crossFade().dontAnimate().into(((ImageView) helper.getView(R.id.image)));
+                helper.setText(R.id.subject, item.getSubject());
+                helper.setText(R.id.course_title, item.getName());
+                helper.setText(R.id.count, String.valueOf(item.getBuy_tickets_count()) + "人已购买");
+            }
+        };
+        gridSame.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(TeacherDataActivity.this, RemedialClassDetailActivity.class);
+                intent.putExtra("id", list2.get(position).getId());
+                startActivity(intent);
+            }
+        });
+        gridSame.setAdapter(adapter2);
+    }
+
+    /**
+     * 相似课程数据初始化
+     */
+    private void initDataSame(final int type) {
+        if (type == 1) {
+            page = 1;
+        }
+        Map<String, String> map = new HashMap<>();
+        map.put("page", String.valueOf(page));
+        map.put("per_page", "10");
+        map.put("sort_by", "");
+        try {
+            map.put("subject", URLEncoder.encode(subject.getText().toString(), "UTF-8"));
+            map.put("grade", URLEncoder.encode(category.getText().toString(), "UTF-8"));
+            // TODO: 2016/11/15 等待接口 通过category查询辅导班
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        DaYiJsonObjectRequest request = new DaYiJsonObjectRequest(UrlUtils.getUrl(UrlUtils.urlRemedialClass, map), null,
+                new VolleyListener(TeacherDataActivity.this) {
+
+
+                    @Override
+                    protected void onSuccess(JSONObject response) {
+                        if (type == 1) {
+                            list2.clear();
+                        }
+                        scroll.onRefreshComplete();
+
+                        try {
+                            RemedialClassBean data = JsonUtils.objectFromJson(response.toString(), RemedialClassBean.class);
+                            list2.addAll(data.getData());
+                            adapter2.notifyDataSetChanged();
+                        } catch (JsonSyntaxException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    protected void onError(JSONObject response) {
+                        scroll.onRefreshComplete();
+                    }
+
+                    @Override
+                    protected void onTokenOut() {
+                        tokenOut();
+                    }
+                }, new VolleyErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                super.onErrorResponse(volleyError);
+                scroll.onRefreshComplete();
+            }
+        });
+
+        addToRequestQueue(request);
     }
 
     /**
@@ -122,7 +224,7 @@ public class TeacherDataActivity extends BaseActivity {
                     protected void onSuccess(JSONObject response) {
                         Logger.e(response.toString());
                         if (type == 1) {
-                            list.clear();
+                            list1.clear();
                         }
                         try {
                             TeacherDataBean bean = JsonUtils.objectFromJson(response.toString(), TeacherDataBean.class);
@@ -143,9 +245,9 @@ public class TeacherDataActivity extends BaseActivity {
                                 sex.setTextColor(getSexColor(bean.getData().getGender()));
                                 Glide.with(TeacherDataActivity.this).load(bean.getData().getAvatar_url()).placeholder(R.mipmap.error_header_rect).crossFade().into(headSculpture);
                                 school.setText(bean.getData().getSchool());
-                                list.addAll(bean.getData().getCourses());
+                                list1.addAll(bean.getData().getCourses());
                             }
-                            adapter.notifyDataSetChanged();
+                            adapter1.notifyDataSetChanged();
                         } catch (JsonSyntaxException e) {
                             e.printStackTrace();
                         }
@@ -189,11 +291,11 @@ public class TeacherDataActivity extends BaseActivity {
     private String getTeachingYear(String teaching_years) {
         switch (teaching_years) {
             case "within_three_years":
-                return getResourceString(R.string.within_three_years)+"教龄";
+                return getResourceString(R.string.within_three_years) + "教龄";
             case "within_ten_years":
-                return getResourceString(R.string.within_ten_years)+"教龄";
+                return getResourceString(R.string.within_ten_years) + "教龄";
             case "within_twenty_years":
-                return getResourceString(R.string.within_twenty_years)+"教龄";
+                return getResourceString(R.string.within_twenty_years) + "教龄";
         }
         return "";
     }
