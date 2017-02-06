98a455dc6c5d90b06747d9013b260bfbf1ca00dc
lungtify <15617685965@163.com>
2017-02-06 10:27:02 +0800

    QTA-229 图片大小限制

--- app/src/main/java/cn/qatime/player/activity/PictureSelectActivity.java
+++ app/src/main/java/cn/qatime/player/activity/PictureSelectActivity.java
@@ -3,8 +3,6 @@ package cn.qatime.player.activity;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
