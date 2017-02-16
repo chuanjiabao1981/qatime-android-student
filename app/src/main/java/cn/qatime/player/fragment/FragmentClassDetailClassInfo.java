9a8b3edaa02f441f2c8c7b27fe6184aea54c3df1
TianhaoranLY <tianhaoran@bjwwtd.com>
2017-02-16 14:23:26 +0800

    QTA-247 webView显示富文本

--- app/src/main/java/cn/qatime/player/fragment/FragmentClassDetailClassInfo.java
+++ app/src/main/java/cn/qatime/player/fragment/FragmentClassDetailClassInfo.java
@@ -1,10 +1,13 @@
 package cn.qatime.player.fragment;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 import java.text.ParseException;
@@ -17,7 +20,7 @@ import libraryextra.utils.StringUtils;
 
 public class FragmentClassDetailClassInfo extends BaseFragment {
 
-    TextView describe;
+    WebView describe;
     TextView classStartTime;
     TextView classEndTime;
     TextView subject;
@@ -45,8 +48,26 @@ public class FragmentClassDetailClassInfo extends BaseFragment {
         classStartTime = (TextView) view.findViewById(R.id.class_start_time);
         classEndTime = (TextView) view.findViewById(R.id.class_end_time);
         totalclass = (TextView) view.findViewById(R.id.total_class);
-        describe = (TextView) view.findViewById(R.id.describe);
         classType = (TextView) view.findViewById(R.id.class_type);
+        describe = (WebView) view.findViewById(R.id.describe);
+
+        describe.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                return true;
+            }
+        });
+
+        describe.setBackgroundColor(0); // 设置背景色
+        describe.getBackground().setAlpha(0); // 设置填充透明度 范围：0-255
+        describe.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY); //取消滚动条白边效果
+        WebSettings settings = describe.getSettings();
+        settings.setDefaultTextEncodingName("UTF-8") ;
+        settings.setBlockNetworkImage(false);
+        settings.setDefaultFontSize(13);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            settings.setMixedContentMode(settings.MIXED_CONTENT_ALWAYS_ALLOW);  //注意安卓5.0以上的权限
+        }
     }
 
     public void setData(RemedialClassDetailBean bean) {
@@ -60,7 +81,11 @@ public class FragmentClassDetailClassInfo extends BaseFragment {
             }
             grade.setText((bean.getData().getGrade() == null ? "" : bean.getData().getGrade()));
             totalclass.setText(getString(R.string.lesson_count, bean.getData().getPreset_lesson_count()));
-            describe.setText(StringUtils.isNullOrBlanK(bean.getData().getDescription()) ? getString(R.string.no_desc) : bean.getData().getDescription());
+            String body =StringUtils.isNullOrBlanK(bean.getData().getDescription()) ? getString(R.string.no_desc) : bean.getData().getDescription();
+            body = body.replace("\r\n", "<br />");
+            body="<android>"+body+"</android>";
+            String css = "<style>android {color:#999999;}</style>";//默认color
+            describe.loadDataWithBaseURL(null,css+body,"text/html","UTF-8",null);
         }
     }
 }
