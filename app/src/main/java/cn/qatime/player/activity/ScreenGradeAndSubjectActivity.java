19c2fe41a5df3ea18cecda506c1cc0bc8ed800f7
lungtify <15617685965@163.com>
2017-03-14 19:29:47 +0800

    QTA-276 筛选班级和科目页面

---
+++ app/src/main/java/cn/qatime/player/activity/ScreenGradeAndSubjectActivity.java
@@ -0,0 +1,25 @@
+package cn.qatime.player.activity;
+
+import android.os.Bundle;
+
+import cn.qatime.player.R;
+import cn.qatime.player.base.BaseActivity;
+
+/**
+ * @author lungtify
+ * @Time 2017/3/14 15:29
+ * @Describe
+ */
+
+public class ScreenGradeAndSubjectActivity extends BaseActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.);
+        setTitles("选课");
+
+
+    }
+}
