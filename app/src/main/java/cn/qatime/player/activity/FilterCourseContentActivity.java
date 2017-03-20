1b10dc6974c2cd4d0861aaf32b05222d92e9dd47
lungtify <15617685965@163.com>
2017-03-20 15:23:45 +0800

    QTA-276 筛选数据填充

--- app/src/main/java/cn/qatime/player/activity/FilterCourseContentActivity.java
+++ app/src/main/java/cn/qatime/player/activity/FilterCourseContentActivity.java
@@ -4,22 +4,40 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
 import com.handmark.pulltorefresh.library.PullToRefreshListView;
 
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import cn.qatime.player.R;
 import cn.qatime.player.base.BaseActivity;
+import cn.qatime.player.bean.FilterCourseContentBean;
 import cn.qatime.player.utils.Constant;
+import cn.qatime.player.utils.DaYiJsonObjectRequest;
+import cn.qatime.player.utils.UrlUtils;
 import libraryextra.adapter.CommonAdapter;
 import libraryextra.adapter.ViewHolder;
+import libraryextra.utils.JsonUtils;
+import libraryextra.utils.StringUtils;
+import libraryextra.utils.VolleyErrorListener;
+import libraryextra.utils.VolleyListener;
 
 /**
  * @author lungtify
@@ -33,12 +51,18 @@ public class FilterCourseContentActivity extends BaseActivity {
     private String grade;
     private String subject;
     private PullToRefreshListView listview;
-    private CommonAdapter<Object> adapter;
-    private List<Object> datas = new ArrayList<>();
+    private CommonAdapter<FilterCourseContentBean.DataBean> adapter;
+    private List<FilterCourseContentBean.DataBean> datas = new ArrayList<>();
     private View screen;
     private int latestResult = 1;//0上1下-1未选
     private int popularityResult = -1;
     private int priceResult = -1;
+    private int page = 1;
+    private String tag = null;//标签
+    private String range = null;//查询范围
+    private String courseStatus = null;//课程状态
+    private String endTime = null;
+    private String startTime = null;
 
 
     @Override
@@ -49,7 +73,93 @@ public class FilterCourseContentActivity extends BaseActivity {
         subject = getIntent().getStringExtra("subject");
         setTitles(grade + subject);
         initView();
+        getData(0);
+    }
 
+    /**
+     * @param type 0下拉1上啦
+     */
+    private void getData(final int type) {
+        Map<String, String> map = new HashMap<>();
+        map.put("per_page", "20");
+        map.put("page", String.valueOf(page));
+
+        if (latestResult != -1) {//0上1下-1未选
+            if (latestResult == 0) {
+                map.put("sort_by", "published_at");
+            } else {
+                map.put("sort_by", "published_at.asc");
+            }
+        } else if (priceResult != -1) {
+            if (priceResult == 0) {
+                map.put("sort_by", "price");
+            } else {
+                map.put("sort_by", "price.asc");
+            }
+        } else if (popularityResult != -1) {
+            if (popularityResult == 0) {
+                map.put("sort_by", "buy_tickets_count");
+            } else {
+                map.put("sort_by", "buy_tickets_count.asc");
+            }
+        }
+        try {
+            map.put("q[grade_eq]", URLEncoder.encode(grade, "UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (!subject.equals("全部")) {
+                map.put("q[subject_eq]", URLEncoder.encode(subject, "UTF-8"));
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        if (!StringUtils.isNullOrBlanK(tag)) {
+            try {
+                map.put("tags", URLEncoder.encode(tag, "UTF-8"));
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+        if (!StringUtils.isNullOrBlanK(range)) {
+            map.put("range", range);
+        }
+        if (!StringUtils.isNullOrBlanK(courseStatus)) {
+            map.put("q[status_eq]", courseStatus);
+        }
+        if (!StringUtils.isNullOrBlanK(startTime) && !StringUtils.isNullOrBlanK(endTime)) {
+            map.put("q[class_date_gteq]", startTime);
+            map.put("q[class_date_lt]", endTime);
+        }
+
+        DaYiJsonObjectRequest request = new DaYiJsonObjectRequest(UrlUtils.getUrl(UrlUtils.urlSearch, map), null, new VolleyListener(this) {
+            @Override
+            protected void onTokenOut() {
+
+            }
+
+            @Override
+            protected void onSuccess(JSONObject response) {
+                if (type == 0) {
+                    datas.clear();
+                }
+                String label = DateUtils.formatDateTime(FilterCourseContentActivity.this, System.currentTimeMillis(), DateUtils.FORMAT_SHOW_TIME | DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_ABBREV_ALL);
+                listview.getLoadingLayoutProxy(true, false).setLastUpdatedLabel(label);
+                listview.onRefreshComplete();
+                FilterCourseContentBean data = JsonUtils.objectFromJson(response.toString(), FilterCourseContentBean.class);
+                assert data != null;
+                datas.addAll(data.getData());
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            protected void onError(JSONObject response) {
+
+            }
+        }, new VolleyErrorListener());
+        addToRequestQueue(request);
     }
 
     private void initView() {
@@ -72,6 +182,7 @@ public class FilterCourseContentActivity extends BaseActivity {
                 refreshState(latest, latestResult);
                 refreshState(price, priceResult);
                 refreshState(popularity, popularityResult);
+                getData(0);
             }
         });
         price.setOnClickListener(new View.OnClickListener() {
@@ -89,7 +200,7 @@ public class FilterCourseContentActivity extends BaseActivity {
                 refreshState(latest, latestResult);
                 refreshState(price, priceResult);
                 refreshState(popularity, popularityResult);
-
+                getData(0);
             }
         });
         popularity.setOnClickListener(new View.OnClickListener() {
@@ -107,29 +218,49 @@ public class FilterCourseContentActivity extends BaseActivity {
                 refreshState(latest, latestResult);
                 refreshState(price, priceResult);
                 refreshState(popularity, popularityResult);
+                getData(0);
             }
         });
         final TextView label = (TextView) findViewById(R.id.label);
         screen = findViewById(R.id.screen);//筛选按钮
 
         listview = (PullToRefreshListView) findViewById(R.id.listview);
-//        datas.add("");
-//        datas.add("");
-//        datas.add("");
-//        datas.add("");
-//        datas.add("");
-//        datas.add("");
-//        datas.add("");
-//        datas.add("");
-//        datas.add("");
-//        datas.add("");
-        adapter = new CommonAdapter<Object>(this, datas, R.layout.item_filter_course) {
+        listview.setMode(PullToRefreshBase.Mode.BOTH);
+        listview.getLoadingLayoutProxy(true, false).setPullLabel(getResources().getString(R.string.pull_to_refresh));
+        listview.getLoadingLayoutProxy(false, true).setPullLabel(getResources().getString(R.string.pull_to_load));
+        listview.getLoadingLayoutProxy(true, false).setRefreshingLabel(getResources().getString(R.string.refreshing));
+        listview.getLoadingLayoutProxy(false, true).setRefreshingLabel(getResources().getString(R.string.loading));
+        listview.getLoadingLayoutProxy(true, false).setReleaseLabel(getResources().getString(R.string.release_to_refresh));
+        listview.getLoadingLayoutProxy(false, true).setReleaseLabel(getResources().getString(R.string.release_to_load));
+        adapter = new CommonAdapter<FilterCourseContentBean.DataBean>(this, datas, R.layout.item_filter_course) {
             @Override
-            public void convert(ViewHolder holder, Object item, int position) {
-
+            public void convert(ViewHolder holder, FilterCourseContentBean.DataBean item, int position) {
+                Glide.with(FilterCourseContentActivity.this).load(item.getPublicize()).crossFade().fitCenter().into((ImageView) holder.getView(R.id.image));
+                holder.setText(R.id.name, item.getName())
+                        .setText(R.id.price, "￥" + item.getPrice())
+                        .setText(R.id.teacher, item.getTeacher_name());
             }
         };
         listview.setAdapter(adapter);
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(FilterCourseContentActivity.this, RemedialClassDetailActivity.class);
+                intent.putExtra("id", datas.get(position - 1).getId());
+                startActivity(intent);
+            }
+        });
+        listview.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener2<ListView>() {
+            @Override
+            public void onPullDownToRefresh(PullToRefreshBase<ListView> refreshView) {
+                getData(0);
+            }
+
+            @Override
+            public void onPullUpToRefresh(PullToRefreshBase<ListView> refreshView) {
+                getData(1);
+            }
+        });
         final List<String> labelData = new ArrayList<>();
         labelData.add("不限");
         labelData.add("高考");
@@ -175,6 +306,12 @@ public class FilterCourseContentActivity extends BaseActivity {
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                         label.setText(labelData.get(position));
+                        if (labelData.get(position).equals("不限")) {
+                            tag = null;
+                        } else {
+                            tag = labelData.get(position);
+                        }
+                        getData(0);
                         dialog.dismiss();
                     }
                 });
@@ -187,6 +324,10 @@ public class FilterCourseContentActivity extends BaseActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(FilterCourseContentActivity.this, ScreeningConditionActivity.class);
+                intent.putExtra("range", range);
+                intent.putExtra("courseStatus", courseStatus);
+                intent.putExtra("startTime", startTime);
+                intent.putExtra("endTime", endTime);
                 startActivityForResult(intent, Constant.REQUEST);
             }
         });
@@ -212,6 +353,14 @@ public class FilterCourseContentActivity extends BaseActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
+        if (requestCode == Constant.REQUEST && resultCode == Constant.RESPONSE) {
+            if (data != null) {
+                range = data.getStringExtra("range");
+                courseStatus = data.getStringExtra("courseStatus");
+                startTime = data.getStringExtra("startTime");
+                endTime = data.getStringExtra("endTime");
+                getData(0);
+            }
+        }
     }
 }
