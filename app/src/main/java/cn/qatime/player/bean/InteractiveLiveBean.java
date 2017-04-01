37135734c4dff87700df331c7b8a908086a06bc2
lungtify <15617685965@163.com>
2017-04-01 20:02:57 +0800

    QTA-288 聊天初始化，详情数据

---
+++ app/src/main/java/cn/qatime/player/bean/InteractiveLiveBean.java
@@ -0,0 +1,12 @@
+package cn.qatime.player.bean;
+
+import java.io.Serializable;
+
+/**
+ * @author lungtify
+ * @Time 2017/3/31 15:31
+ * @Describe
+ */
+
+public class InteractiveLiveBean implements Serializable {
+}
